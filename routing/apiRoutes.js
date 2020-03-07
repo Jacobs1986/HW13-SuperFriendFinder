bdf0f4145fdeb60499c2e48082ce6681d06ca6ef
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-07 16:42:43 -0700

    Added the POST method to the apiRoutes.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,4 +5,11 @@ module.exports = function(app) {
     app.get("/api/friends", function(request, result) {
         return result.json(heroArray);
     })
+    // post the new friends information to the array
+    app.post("/api/friends", function(request, result) {
+        newFriend = request.body;
+        console.log(newFriend);
+        heroArray.push(newFriend);
+        result.json(newFriend);
+    })
 }
