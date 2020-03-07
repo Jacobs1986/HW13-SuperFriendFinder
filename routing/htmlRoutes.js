025f58e8b03833b18e7d3047c633a098eeb0cc40
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-07 15:56:01 -0700

    I moved the questions.js out of the data files and into the public folder. I had to do some research on how to link it so that it would be read and loaded into the survey.html.

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,5 +1,7 @@
+const path = require("path");
+
 module.exports = function(app) {
-    app.get("/", function(reqeust, result) {
-        
+    app.get("/survey", function(reqeust, result) {
+        result.sendFile(path.join(`${__dirname}/../public`, "survey.html"))
     })
 }
--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+let routes = require('./routing/htmlRoutes')(app)
+
+// Routes
+app.get("/", function(request, result) {
+    result.sendFile(path.join(`${__dirname}/public`, "home.html"));
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
