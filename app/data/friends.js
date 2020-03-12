0fc443960529fe3bd1175cc80fb3fac3ebd9a237
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-12 16:07:50 -0600

    Added Batman to the hero array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,17 @@ let heros = [
     {
         name: "Superman",
         photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/a/a8/Action_Comics_Vol_1_1000_Textless.jpg/revision/latest?cb=20180411074920",
-        values: [5, 5, 2, 3, 4]
+        values: [5, 5, 2, 4, 3, 2, 3, 2, 2, 3]
     },
     {
         name: "Wonder Woman",
         photo: "https://cdn11.bigcommerce.com/s-0kvv9/images/stencil/1280x1280/products/304817/434376/jul190616__30485.1565886683.jpg?c=2&imbypass=on",
-        values: [4, 2, 5, 2, 5]
+        values: [5, 3, 4, 3, 4, 4, 2, 2, 1, 5]
+    },
+    {
+        name: "Batman",
+        photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/a/a6/Batman_Vol_2_2_Variant_Textless.jpg/revision/latest/top-crop/width/360/height/450?cb=20120228075313",
+        values: [1, 3, 4, 5, 2, 3, 1, 2, 4, 4]
     }
 ]
 
