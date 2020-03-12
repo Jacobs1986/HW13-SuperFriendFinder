a12e5833c139099056d22877990fbdc917283137
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-12 10:57:07 -0600

    Created an assets folder that will hold the js and css files for the project.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static("public"));
 
 // Routes
 
@@ -17,5 +17,5 @@ require('./routing/htmlRoutes')(app)
 require('./routing/apiRoutes')(app)
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on http://localhost:" + PORT);
 });
